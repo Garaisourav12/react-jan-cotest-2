68069e1f35752d7f11fe128fabc4c40df69cf6c8
Garaisourav12 <garaisourav12@gmail.com>
2024-01-20 13:23:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Account from "./components/Account";
+import Forget from "./components/Forget";
+
+const App =()=>{
+    return (
+        <div className="main-div">
+            <Login />
+            <Forget />
+            <Account />
+            {/* <Profile /> */}
+        </div>
+    )
+}
+export default App;
